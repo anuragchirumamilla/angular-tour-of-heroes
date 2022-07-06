d7fc376ad371d83dbe7374e582a7eb0abb8a332d
NagaSaiAnurag <108649481+anuragchirumamilla@users.noreply.github.com>
2022-07-06 17:13:57 +0530

    updated till navigation half

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -23,7 +23,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+    this.messageService.add(`Selected hero ID= ${hero.id} & Name= ${hero.name}`);
   }
 
   getHeroes(): void {
